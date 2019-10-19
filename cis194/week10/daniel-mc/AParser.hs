804f6f0684c0c4f7ceeb41fb7d2aa76ec6b91441
Daniel McMillan <mcmillan.daniel3@gmail.com>
2019-10-19 14:00:19 +1100

    week10 exercise 2

--- cis194/week10/daniel-mc/AParser.hs
+++ cis194/week10/daniel-mc/AParser.hs
@@ -21,13 +21,13 @@ newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 -- does not satisfy the predicate (or an empty input), it fails.
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
-  where
-    f [] = Nothing    -- fail on the empty input
-    f (x:xs)          -- check if x satisfies the predicate
-                        -- if so, return x along with the remainder
-                        -- of the input (that is, xs)
-        | p x       = Just (x, xs)
-        | otherwise = Nothing  -- otherwise, fail
+ where
+  f [] = Nothing    -- fail on the empty input
+  f (x : xs) |          -- check if x satisfies the predicate
+                      -- if so, return x along with the remainder
+                      -- of the input (that is, xs)
+               p x       = Just (x, xs)
+             | otherwise = Nothing  -- otherwise, fail
 
 -- Using satisfy, we can define the parser 'char c' which expects to
 -- see exactly the character c, and fails otherwise.
@@ -49,11 +49,10 @@ Just ('x',"yz")
 -- integers.
 posInt :: Parser Integer
 posInt = Parser f
-  where
-    f xs
-      | null ns   = Nothing
-      | otherwise = Just (read ns, rest)
-      where (ns, rest) = span isDigit xs
+ where
+  f xs | null ns   = Nothing
+       | otherwise = Just (read ns, rest)
+    where (ns, rest) = span isDigit xs
 
 ------------------------------------------------------------
 -- Your code goes below here
@@ -61,9 +60,19 @@ posInt = Parser f
 
 -- Exercise 1
 
-first :: (a -> b) -> (a,c) -> (b,c)
-first f = swap . (fmap f) . swap
+first :: (a -> b) -> (a, c) -> (b, c)
+first f = swap . fmap f . swap
 
 instance Functor Parser where
-  fmap f (Parser run1) = Parser run2
-    where run2 = fmap (first f) . run1
+  fmap f (Parser run1) = Parser run2 where run2 = fmap (first f) . run1
+
+-- Exercise 2
+
+flatMaybe :: Maybe (Maybe a) -> Maybe a
+flatMaybe (Just x) = x
+flatMaybe Nothing  = Nothing
+
+instance Applicative Parser where
+  pure a = Parser (\s -> Just (a, s))
+  p1 <*> p2 = Parser $ (=<<) f . runParser p1 where f (p1f, rem) = runParser (p1f <$> p2) rem
+  -- p1 <*> p2 = Parser $ ((=<<) . uncurry) (runParser . (<$> p2)) . runParser p1
